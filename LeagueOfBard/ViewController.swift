975f68bf0d46b49716ace1234e13a6b53c9fad68
iMacFabLab <imacfablab@iMac-de-iMacFabLab.local>
2017-01-10 11:43:44 +0100

    Initial Commit

---
+++ LeagueOfBard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeagueOfBard
+//
+//  Created by iMacFabLab on 10/01/2017.
+//  Copyright © 2017 iMacFabLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

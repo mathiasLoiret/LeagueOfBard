62ffbc3da9801c1f234f402bd9237a30d1e859d3
mathiasLoiret <mathias44.loiret@hotmail.fr>
2017-02-22 15:48:25 +0100

    first commit

---
+++ LeagueOfBard/ChampHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  ChampHeaderView.swift
+//  LeagueOfBard
+//
+//  Created by iMacFabLab on 16/01/2017.
+//  Copyright © 2017 iMacFabLab. All rights reserved.
+//
+
+import Foundation
---
+++ LeagueOfBard/InfosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfosViewController.swift
+//  LeagueOfBard
+//
+//  Created by iMacFabLab on 17/01/2017.
+//  Copyright © 2017 iMacFabLab. All rights reserved.
+//
+
+import UIKit
+
+class InfosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
